aa20acaf2ba4c3992dfc2ae50a4995c3b04926a9
sterben04 <aravindunni00@gmail.com>
2021-10-07 20:13:04 +0530

    Added a few more algorithms

---
+++ Algorithms/selectionSort.py
@@ -0,0 +1,22 @@
+"""
+Selection sort searches for the minimum element in an array (ascending order) and
+places it at the front of the array.
+
+Time Complexity:
+Best, Avg, Worst = O(n**2)
+
+"""
+
+def selectionSort(arr):
+    l = len(arr)
+    for i in range(l-1):
+        #first i elements are already sorted
+        for j in range(i+1,l): 
+            if arr[i]>arr[j]:
+                #swap
+                arr[j], arr[i] = arr[i], arr[j]
+    return arr
+
+if __name__ == "__main__":
+    arr = list(map(int, input().split()))
+    print(selectionSort(arr))
