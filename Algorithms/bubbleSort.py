c63abed5a4eed9284fce04e9bfb1d127722e8f1a
sterben04 <aravindunni00@gmail.com>
2021-10-07 10:48:03 +0530

    added bubble sort

---
+++ Algorithms/bubbleSort.py
@@ -0,0 +1,20 @@
+arr = list(map(int, input().split()))
+
+n = len(arr)
+
+"""
+In bubble sort adjacent elements are compared and are swapped if they are not in order.
+
+Time complexity:
+Best - O(n)
+Avg - O(n**2)
+Worst  - O(n**2)
+"""
+
+
+for i in range(n):
+    for j in range(n-i-1):    # last i elements are already in place
+        if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+print(arr)
